392abc8d5acd1a723ea2382dc9257498e027030a
Roman Druzhinin <karamount11@gmail.com>
2024-10-03 08:22:27 +0300

    feature: useThrottle hook

---
+++ src/shared/hooks/index.ts
@@ -0,0 +1,3 @@
+export { useThrottle } from "./useThrottle/useThrottle";
+export { useTypedSelector } from "./useTypedSelector/useTypedSelector";
+export { useTypedDispatch } from "./useTypesDispatch/useTypedDispatch";
