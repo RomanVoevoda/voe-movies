107a9f4225c9438d8a0ca1a42c47f9b93abe3751
Roman Druzhinin <karamount11@gmail.com>
2024-10-04 17:00:25 +0300

    fix: back missed next/images config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ["image/webp"],
+    formats: ["image/avif", "image/webp"],
     deviceSizes: [375, 600, 768, 992, 1200, 1920],
     remotePatterns: [
       {
